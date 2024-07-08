6948b15086966bfab981a68412edb2be2ae5483c
Chanpreet Singh <chanpreet3000@gmail.com>
2024-07-08 18:35:02 +0530

    added 170 more problems

--- leetcoder/LeetcoderScraper.js
+++ leetcoder/LeetcoderScraper.js
@@ -101,7 +101,7 @@ class LeetcoderScraper {
     Logger.error('<<<< Starting Leetcoder Scrapper >>>>');
     const {browser} = getBrowserDetails();
     try {
-      let id_no = 700004980
+      let id_no = 700016650
       while (id_no > 0) {
         const promises = [];
         for (let idx = 0; idx < 30; idx++) {
